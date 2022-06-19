ec6ced6316592391634dea195de7ff24129dea97
yousafsabir <yousafsabir430516@gmail.com>
2022-06-20 01:37:27 +0500

    primitives, any, for one type array, tuples

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+// Types
+// Primitive Types
+let age = 19;
+let profession = "Developer";
+let isGoodAtIt;
+// 🤔🤔🤔🤔
+// 😊😊😉😊
+isGoodAtIt = true;
+let doingJob = false;
+// Any Type
+let x = "It could be assigned or re-assigned with any type of data";
+x = true;
+// Dealing with array
+// An array only supporting numbers
+let numbers = [1, 2, 3, 1];
+// array supporting any type
+let arr = [1, true, "hello"];
+// Tuples
+// types for exact spots
+let person = [1, "Yousaf", true];
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+// Types
+// Primitive Types
+let age: number = 19;
+let profession: string = "Developer";
+let isGoodAtIt: boolean;
+// 🤔🤔🤔🤔
+// 😊😊😉😊
+isGoodAtIt = true;
+let doingJob: boolean = false;
+
+// Any Type
+let x: any = "It could be assigned or re-assigned with any type of data";
+x = true;
+
+// Dealing with array
+// An array only supporting numbers
+let numbers: number[] = [1, 2, 3, 1];
+// array supporting any type
+let arr: any[] = [1, true, "hello"];
+
+// Tuples
+// types for exact spots
+let person: [number, string, boolean] = [1, "Yousaf", true];
